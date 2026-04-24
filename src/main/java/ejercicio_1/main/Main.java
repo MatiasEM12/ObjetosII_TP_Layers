28fdade31047b5c77521cbedd770b0466933907b
MatiasEM12 <matiasemimellado@gmail.com>
2026-04-24 14:34:07 -0300

    bases ejericio 1

---
+++ src/main/java/ejercicio_1/main/Main.java
@@ -0,0 +1,19 @@
+package ejercicio_1.main;
+
+import java.awt.*;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new AgregarParticipante();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+    }
+
+}
---
+++ src/main/java/ejercicio_1/modelo/Participante.java
@@ -0,0 +1,4 @@
+package ejercicio_1.modelo;
+
+public class Participante {
+}
